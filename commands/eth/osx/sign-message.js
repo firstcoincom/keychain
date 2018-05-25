63b3b6c1c2ee8a1283697bfaa54ee8c3742071dc
Kartik <kartik@firstcoin.com>
2018-05-24 19:47:24 -0700

    adding method to sign message

---
+++ commands/eth/osx/sign-message.js
@@ -0,0 +1,38 @@
+const prompt = require('prompt');
+const readFiles = require('read-files-promise');
+
+const eth = require('../../../lib/eth');
+
+const logError = err => {
+  if (err) {
+    console.log(err);
+    process.exit(-1);
+  }
+}
+
+const signMessage = (walletName, usbNumbers, message) => {
+  const splitFiles = usbNumbers.trim().split(' ').map(num => `/Volumes/${walletName}-${num}/split.txt`);
+  readFiles(splitFiles, {encoding: 'utf8'})
+  .then(
+    shares => {
+      console.log("\n\n");
+      console.log(eth.signMessage(shares, message));
+      console.log("\n\n");
+    },
+    logError,
+  ).catch(logError);
+}
+
+prompt.start();
+prompt.get([
+  'walletName',
+  'usbNumbers',
+  'message'
+], (err, results) => {
+  if (err) {
+    console.log("unable to read inputs");
+    return;
+  }
+
+  signMessage(results.walletName, results.usbNumbers, results.message);
+});
