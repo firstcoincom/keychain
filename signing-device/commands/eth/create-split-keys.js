85d632e333c35223e56ce49a10e1bca2655ab2b4
jude <jude@firstcoin.com>
2018-06-15 14:52:39 -0700

    save

--- signing-device/commands/eth/create-split-keys.js
+++ signing-device/commands/eth/create-split-keys.js
@@ -59,7 +59,7 @@ const createSplitKeysAndVerifyResults = (walletName, entropy, numShares, thresho
   logText("writing split keys");
   const files = [];
   const addressQRPath = "/home/pi/addrQR.txt"
-  utils.genQRCode(data.address, addressQRPath);
+  //utils.genQRCode(data.address, addressQRPath);
   data.shares.forEach((share, index) => {
     const splitFilename =`${usbBasePath}/${walletName}-${index + 1}/split.txt`;
     files.push(splitFilename);
