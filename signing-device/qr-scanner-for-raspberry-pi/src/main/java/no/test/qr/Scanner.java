0878baf56ac49ab41c42d9f1573fdddf1a88cfb8
jude <jude@firstcoin.com>
2018-06-15 13:40:34 -0700

    save

--- signing-device/qr-scanner-for-raspberry-pi/src/main/java/no/test/qr/Scanner.java
+++ signing-device/qr-scanner-for-raspberry-pi/src/main/java/no/test/qr/Scanner.java
@@ -44,8 +44,8 @@ public class Scanner {
 	    bw.write(result);
 
         System.out.println(result);
-        jsonContent = result.replaceAll("\\$*\\$", "");
-        fileName = result.replaceAll("^\\$","").replaceAll("*\\$","");
+        jsonContent = result.replaceAll("\\$.*\\$", "");
+        fileName = result.replaceAll("^\\$","").replaceAll(".*\\$","");
 
         System.out.println(jsonContent);
         System.out.println(fileName);
